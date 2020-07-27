956ba76645d61e814c444418427e43095dd58e2a
Josh Nunez <joshjnunez09@gmail.com>
2020-07-27 15:44:34 -0500

    connected to database - mongoDB

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const { PORT } = process.env;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/mvp-demo-2020')
+  .then(() => {
+    console.log('Connected to the database!!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the DB:', err);
+  });
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
