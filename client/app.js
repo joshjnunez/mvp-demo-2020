2b7ccc6c2919b8ef27d1f871abad592b17ab0838
Josh Nunez <joshjnunez09@gmail.com>
2020-07-27 16:06:05 -0500

    webpack/react installed

---
+++ client/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Page from './page';
+
+render(<Page />, document.getElementById('app-root'));
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './client/app',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
